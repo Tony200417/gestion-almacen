03bfedf7b219821144e3796359d8743f84e4c234
cbaciliodev <dbacilio88@outlook.es>
2023-12-01 19:57:37 -0500

    init project
    Init project

---
+++ src/main/java/bacilio/main/TareaJose.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bacilio.main;
+
+/**
+ *
+ * @author cbaciliod
+ */
+public class TareaJose {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/bacilio/repositories/entities/Persona.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bacilio.repositories.entities;
+
+/**
+ *
+ * @author cbaciliod
+ */
+public class Persona {
+    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
